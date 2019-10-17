4f82dcb2ac0867d5c0d5d63bc66d337cdafe7c3c
frederikbraagaard <frederikbraagaard@192.168.0.12>
2019-10-17 16:56:36 +0200

    Revert "git ignore test"
    This reverts commit 6baecd2f2e957183c8abae11817630043dd1b81a.

--- CupcakeProject/src/main/java/persistence/DBConnection.java
+++ CupcakeProject/src/main/java/persistence/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection {
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
 
-        String dbproperties = "/PATH/TO/DB.PROPERTIESs";
+        String dbproperties = "/PATH/TO/DB.PROPERTIES";
 
         try (FileInputStream f = new FileInputStream(dbproperties)) {
 
