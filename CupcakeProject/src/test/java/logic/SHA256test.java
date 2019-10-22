8d798a9c880afeff17f7eb7e9b3fd84aab7f7007
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-10-22 21:45:22 +0200

    Added some JUnit tests.

---
+++ CupcakeProject/src/test/java/logic/SHA256test.java
@@ -0,0 +1,33 @@
+package logic;
+
+import logic.*;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.DatatypeConverter;
+
+
+public class SHA256test {
+    
+    /**
+     * @author Bringordie - Frederik Braagaard
+     * @return returns HexBinary SHA-256 encryption of an password used for
+     * adding password to the DB but also used for login to check match.
+    */
+    public static String getHash(byte[] inputBytes) {
+        String hashValue = "";
+        
+        try {
+            //Static getInstance methos is called with hashing SHA-256
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(inputBytes);
+            byte[] digestedByte = md.digest();
+            hashValue = DatatypeConverter.printHexBinary(digestedByte).toLowerCase();
+        } catch (Exception e) {
+            System.err.println("An error has occured");
+        }
+        return hashValue;
+    }
+            
+}
---
+++ CupcakeProject/src/test/java/persistence_demo/CupCakeMapperDemo.java
@@ -0,0 +1,286 @@
+package persistence_demo;
+
+import persistence.*;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import logic.Bottom;
+import logic.SHA256;
+import logic.SHA256test;
+import logic.Topping;
+import static persistence.DBConnection.getConnection;
+import static persistence_demo.DBDemo.getConnection;
+
+
+
+public class CupCakeMapperDemo {
+
+    /**
+     * @author Bringordie - Frederik Braagaard
+     * @param username - Input from Registration.jsp
+     * @param password - Input from Registration.jsp
+     */
+    public static void reqisterUser(String username, String password, String name, String email) throws SQLException, ClassNotFoundException {
+        String encrypt = SHA256.getHash(password.getBytes());
+        String sql = "INSERT INTO users(Username, Password, Role, Name, Email)VALUES(?,?,?,?,?)";
+
+        try {
+            PreparedStatement statement = DBDemo.getConnection().prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, encrypt);
+            statement.setBoolean(3, false);
+            statement.setString(4, name);
+            statement.setString(5, email);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * @author Bringordie - Frederik Braagaard
+     * @param username - Input from Registration.jsp and Login.jsp to see if the
+     * username already exists/is taken.
+     * @return Method will return either true or false depending on the username
+     * already existing in the DB.
+     */
+    public static boolean checkUsername(String username) throws ClassNotFoundException, SQLException {
+        int countTotal = 0;
+        boolean returnAnswer = true;
+        String sql = "select count(username) from users where Username = '" + username + "'";
+        ResultSet result = DBDemo.getConnection().prepareStatement(sql).executeQuery();
+        try {
+            while (result.next()) {
+                int getTotal = result.getInt(1);
+                countTotal = getTotal;
+            }
+        } catch (SQLException ex) {
+            System.err.println("SQException Error");
+        }
+        if (countTotal != 0) {
+            returnAnswer = true;
+        } else {
+            returnAnswer = false;
+        }
+        return returnAnswer;
+    }
+
+    /**
+     * @author Bringordie - Frederik Braagaard
+     * @param username - Uses the username for SQL string
+     * @param password - Password check for login.jsp
+     * @return Method will return either true or false depending on if the
+     * encrypted password matches the one in the DB.
+     */
+    public static boolean checkPassword(String username, String password) throws ClassNotFoundException, SQLException {
+        String hashPassword = SHA256test.getHash(password.getBytes());
+        Boolean passwordBoolean = false;
+        String sql = "select Password from users where Username = '" + username + "'";
+        ResultSet result = DBDemo.getConnection().prepareStatement(sql).executeQuery();
+        try {
+            while (result.next()) {
+                String getHash = result.getString(1);
+                if (getHash.contains(hashPassword)) {
+                    passwordBoolean = true;
+                } else {
+                    passwordBoolean = false;
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CupCakeMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return passwordBoolean;
+    }
+
+    /**
+     * @author Bringordie - Frederik Braagaard
+     * @param username - Role check for access and for navigation after login.
+     * @return Method will return either true or false depending on the role
+     * used to see if they are a regular user or an admin. And where to navigate
+     * them
+     */
+    public static boolean checkRole(String username) throws ClassNotFoundException, SQLException {
+        Boolean roleBoolean = false;
+        String sql = "select Role from users where Username = '" + username + "'";
+        ResultSet result = DBDemo.getConnection().prepareStatement(sql).executeQuery();
+        try {
+            while (result.next()) {
+                int getRole = result.getInt(1);
+                if (getRole != 0) {
+                    roleBoolean = true;
+                } else {
+                    roleBoolean = false;
+                }
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CupCakeMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return roleBoolean;
+    }
+
+    /**
+     * @author Bringordie - Frederik Braagaard
+     * @return returns an arraylist of all the toppings in the DB.
+     */
+    public static ArrayList<Topping> getToppings() throws SQLException, ClassNotFoundException {
+        ArrayList<Topping> toppings = new ArrayList();
+        String sql = "select * from toppings";
+        ResultSet result = null;
+        result = DBDemo.getConnection().prepareStatement(sql).executeQuery();
+
+        try {
+            while (result.next()) {
+                int idTopping = result.getInt(1);
+                String toppingName = result.getString(2);
+                double price = result.getDouble(3);
+                toppings.add(new Topping(toppingName, price, idTopping));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CupCakeMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return toppings;
+    }
+
+    /**
+     * @author Bringordie - Frederik Braagaard
+     * @return returns an arraylist of all the buttoms in the DB.
+     */
+    public static ArrayList<Bottom> getBottoms() throws SQLException, ClassNotFoundException {
+        ArrayList<Bottom> bottoms = new ArrayList();
+        String sql = "select * from bottoms";
+        ResultSet result = null;
+        result = DBDemo.getConnection().prepareStatement(sql).executeQuery();
+
+        try {
+            while (result.next()) {
+                int idButtom = result.getInt(1);
+                String buttomName = result.getString(2);
+                double price = result.getDouble(3);
+                bottoms.add(new Bottom(buttomName, price, idButtom));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CupCakeMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bottoms;
+    }
+
+    /**
+     * @author Bringordie - Frederik Braagaard
+     */
+    public static void updateBalance(String username, double Addbalance) throws SQLException, ClassNotFoundException {
+
+        String sql = "UPDATE users set balance = balance +" + Addbalance + " where Username = '" + username + "'";
+        try {
+            PreparedStatement statement = DBDemo.getConnection().prepareStatement(sql);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+    /**
+     * @author Bringordie - Frederik Braagaard
+     */
+    public static double getBalance(String username) throws SQLException, ClassNotFoundException {
+
+        String sql = "select balance from users where Username = '" + username + "'";
+        ResultSet result = DBDemo.getConnection().prepareStatement(sql).executeQuery();
+        double balancesum = 0;
+
+        try {
+            while (result.next()) {
+                balancesum = result.getDouble(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CupCakeMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return balancesum;
+    }
+    
+    
+        //Drops as a start 3 tables (as they have relation).
+        public static void droptable() throws SQLException, ClassNotFoundException {
+
+        String sql = "DROP TABLE if exists lineitems;";
+        String sql1 = "DROP TABLE if exists completeorders;";
+        String sql2 = "DROP TABLE if exists users;";  
+        
+        try {
+            PreparedStatement statement = DBDemo.getConnection().prepareStatement(sql);
+            PreparedStatement statement1 = DBDemo.getConnection().prepareStatement(sql1);
+            PreparedStatement statement2 = DBDemo.getConnection().prepareStatement(sql2);
+            statement.executeUpdate();
+            statement1.executeUpdate();
+            statement2.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+        //Re-creates the 3 drops above
+    public static void createtable() throws SQLException, ClassNotFoundException {
+        String users = "CREATE TABLE users (" +
+"    idUsers INTEGER not null AUTO_INCREMENT unique," +
+"    Username VARCHAR(45) unique not null," +
+"    Password VARCHAR(200) not null," +
+"    Balance DOUBLE not null DEFAULT 0," +
+"    Name VARCHAR(45)," +
+"    Email VARCHAR(45)," +
+"    Role tinyint," +
+"    primary key (idUsers)" +
+");";
+        
+         String completeorders = "CREATE TABLE completeorders (" +
+"	Ordernumber INTEGER not null AUTO_INCREMENT unique," +
+"    idUsers INTEGER," +
+"    OrderDate TIMESTAMP not null," +
+"    Total_price DOUBLE," +
+"    primary key (ordernumber)," +
+"    FOREIGN KEY (idUsers) REFERENCES users(idUsers)" +
+");";
+         
+         String lineitems = "CREATE TABLE lineitems (" +
+"	OrdreNumber INTEGER," +
+"	idBottoms INTEGER," +
+"    idToppings INTEGER," +
+"    quantity INTEGER," +
+"    idUsers INTEGER," +
+"    Price DOUBLE," +
+"    FOREIGN KEY (idUsers) REFERENCES users(idUsers)," +
+"    FOREIGN KEY (idBottoms) REFERENCES bottoms(idBottoms)," +
+"    FOREIGN KEY (idToppings) REFERENCES toppings(idToppings)" +
+");";
+         
+         try {
+            PreparedStatement statement = DBDemo.getConnection().prepareStatement(users);
+            PreparedStatement statement1 = DBDemo.getConnection().prepareStatement(completeorders);
+            PreparedStatement statement2 = DBDemo.getConnection().prepareStatement(lineitems);
+            statement.executeUpdate();
+            statement1.executeUpdate();
+            statement2.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        
+    }
+    
+    public static int checkHowManyUsersExists() throws ClassNotFoundException, SQLException {
+        String sql = "select count(username) from users;";
+        int countTotal = 0;
+        ResultSet result = DBDemo.getConnection().prepareStatement(sql).executeQuery();
+        try {
+            while (result.next()) {
+                int getTotal = result.getInt(1);
+                countTotal = getTotal;
+            }
+        } catch (SQLException ex) {
+            System.err.println("SQException Error");
+        }
+        return countTotal;
+    }
+}
